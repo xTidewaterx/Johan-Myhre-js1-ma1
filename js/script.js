a36cc6c60cad235d854efa1d584e5ff2ce745c39
xTidewaterx <79268288+xTidewaterx@users.noreply.github.com>
2021-06-20 23:59:08 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,78 @@
+//Question 1
+
+var cat = { 
+    "complain": "Meow!" };
+
+
+    function catComplains () {
+console.log(cat.complain);
+
+
+    }
+
+    catComplains ()
+
+
+
+
+// Question 2 /3/4
+
+
+function UpdateThatThing() {
+
+    {
+        var heading = document.querySelector ("h3");
+        heading.innerHTML = "Updated heading";
+        heading.style.color = "red";
+        heading.style.fontSize= "2em";
+        heading.className = "Subheading";   
+     };
+   }
+   UpdateThatThing();
+   
+
+
+
+
+
+
+// Question 5
+//when I write "document.querySelectorAll" nothing happens
+{
+    var paragraph = document.querySelector ("p");
+  paragraph.style.color = "red";
+}
+
+
+
+
+//Question 7
+
+function loopCat (list) {
+
+    
+for (var i = 0; i <= [cats.length]; i++) {
+	console.log(cats.age);
+}
+
+
+
+
+}
+
+loopCat ()
+
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
